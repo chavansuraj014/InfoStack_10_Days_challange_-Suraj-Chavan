a2be1b5efe11eefc5f268d3d203f3193d4fb4fb3
chavansuraj014 <chavansuraj014@gmail.com>
2024-10-14 15:23:46 +0530

    Add files via upload

---
+++ Day_1- Reverse an Array.py
@@ -0,0 +1,30 @@
+Array=[]
+Array_2=[]
+NUMBER=int(input("HOW MANY NUMBERS DO YOU WANT TO PUT IN THE ARRAY - "))
+for i in range(1,NUMBER+1):
+    print("                         ")
+    print("-------------------------")
+    try:
+       num=int(input(F"ENTER NO {i} - "))
+    except ValueError:
+       print("??? PLEASE ENTER INT VALUE")
+       try:
+         num=int(input(F"ENTER NO {i} - "))
+       except ValueError:
+          print("??? PLEASE ENTER INT VALUE")
+          print("SORRY PLEASE TRY AGAIN")
+          exit()
+
+    Array.append(num)
+print("                          ") 
+print("__________________________")
+print(f"YOUR INPUT IS  - {Array}")
+
+value=len(Array)
+
+for i in range(1,len(Array)+1):
+  num_2=Array[value-i]
+  Array_2.append(num_2)
+print("_________________________")
+print(f"YOUR OUTPUT IS - {Array_2}")
+ 

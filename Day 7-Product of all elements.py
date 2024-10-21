d671740a53e856280832cf2388e235baf268b8cf
chavansuraj014 <chavansuraj014@gmail.com>
2024-10-21 21:41:07 +0530

    Add files via upload

---
+++ Day 7-Product of all elements.py
@@ -0,0 +1,38 @@
+Array=[]
+Array3=[]
+print("                                                        ")
+NUMBER=int(input("HOW MANY NUMBERS DO YOU WANT PUT IN THE ARRAY - "))
+
+if NUMBER>1 and NUMBER<=500:
+    for i in range(1,NUMBER+1):
+        print("          ")
+        print("******************************************")
+        VALUE=int(input(f"ENTER VALUE NO {i} - "))
+        Array.append(i)
+    
+    
+    for i in range(0,NUMBER):
+        Array2=[]
+       
+        C=1
+        for j in Array:
+          if Array[i] != j:
+            Array2.append(j)
+       
+        for K in Array2:
+          
+           C=C*K
+        Array3.append(C)
+    print("                                                                                         ")
+    print("__________________________________________________________________________________________")
+    print(f"---------- YOUR INPUT IS {Array} ----------") 
+    print("                                                                                         ")
+    print(f"---------- YOUR OUTPUT IS {Array3} ----------")
+    print("                                                                                          ")
+            
+        
+        
+       
+    
+    
+             
